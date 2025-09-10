b0271b6a8fca47bbc9c5f26c3af7ea8462e0c824
Renan Kanashiro <ralk@ralk.com.br>
2025-09-10 20:58:21 -0300

    Ajuste na paginação 2

--- src/main/java/com/seuusuario/authservice/controller/ServicoController.java
+++ src/main/java/com/seuusuario/authservice/controller/ServicoController.java
@@ -94,7 +94,7 @@ public class ServicoController {
 public ResponseEntity<Page<Servico>> listarMeusServicos(
         Authentication authentication,
         @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(defaultValue = "5") int size,
         @RequestParam(defaultValue = "id") String sortBy,
         @RequestParam(defaultValue = "desc") String direction
 ) {
